f46dff5f72508384e9bc4d73e8bbd17067c394db
rohitcodism <rohitpaula18i@gmail.com>
2023-11-04 00:11:57 +0530

    Hero-section-wip

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,9 +8,9 @@ export const Navbar = () => {
                 <Typography variant="h5" sx={{fontFamily: "Poppins, sans-serif", fontWeight: "Bold", color:"#01D28E"}}><Link to="/" style={{textDecoration : "none", color:"#01D28E"}}>Restora</Link></Typography>
             </Container>
             <Container sx={{ display : "flex", alignItems : "center"}}>
-                <Link style={{textDecoration : "none"}}><Typography variant="h6" sx={{fontWeight : "regular", mr : "5rem", color : "#000000", "&:hover":{color:"#01D28E"}}}>Menu</Typography></Link>
-                <Link style={{textDecoration: "none"}}><Typography variant="h6" sx={{fontWeight : "regular", mr : "5rem", color : "#000000", "&:hover":{color:"#01D28E"}}}>Order</Typography></Link>
-                <Link style={{textDecoration: "none"}}><Typography variant="h6" sx={{fontWeight : "regular", mr : "5rem", color : "#000000", "&:hover":{color:"#01D28E"}}}>Cart</Typography></Link>
+                <Link to="/menu" style={{textDecoration : "none"}}><Typography variant="h6" sx={{fontWeight : "regular", mr : "5rem", color : "#000000", "&:hover":{color:"#01D28E"}}}>Menu</Typography></Link>
+                <Link to="/order" style={{textDecoration: "none"}}><Typography variant="h6" sx={{fontWeight : "regular", mr : "5rem", color : "#000000", "&:hover":{color:"#01D28E"}}}>Order</Typography></Link>
+                <Link to="/cart" style={{textDecoration: "none"}}><Typography variant="h6" sx={{fontWeight : "regular", mr : "5rem", color : "#000000", "&:hover":{color:"#01D28E"}}}>Cart</Typography></Link>
             </Container>
             <Container sx={{display : "flex", justifyContent : "center", alignItems : "center"}}>
                 <Button variant="contained" sx={{border : "3.5px solid #01D28E", borderRadius : "15px", width : "100px", backgroundColor : "#01D28E", textAlign : "center", fontFamily : "Poppins, sans-serif", fontWeight : "medium", boxShadow : "none", "&:hover":{ backgroundColor : "#FFFFFF", border: "3.5px solid #01D28E", color : "#01D28E", boxShadow : "none"}}}>
