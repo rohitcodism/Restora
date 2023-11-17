a71ebfadd397a79474cb7d912ba50bd13d974e18
rohitcodism <rohitpaula18i@gmail.com>
2023-11-18 00:36:58 +0530

    menu-almost-done

--- src/Components/MenuCard.jsx
+++ src/Components/MenuCard.jsx
@@ -1,17 +1,12 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
-const cardEnlargeStyle = {
-    transform: "scale(1.1)", // Scale the card up 10% on hover
-    transition: "all 0.2s ease-in-out", // Smooth transition for the enlarge effect
-};
-
 export const MenuCard = ({ foodTitle, foodDescription, foodImage }) => {
     return (
-            <Card sx={{ width: "250px", height: "300px", borderRadius: "15px", background: "#ffffff", bottom : "2rem" }}>
+            <Card sx={{ width: "250px", height: "300px", borderRadius: "15px", background: "#ffffff", bottom : "2rem", cursor : "pointer", color: "#01D28E", transition : "backgroundColor 0.5s ease-in-out, color 0.2s ease-in-out", "&:hover" : {backgroundColor : "#01D28E", color : "#ffffff"}}}>
                 <CardMedia component="img" height="60%" image={foodImage} alt={foodTitle} />
-                <CardContent sx={{ height: "40%" }}>
-                    <Typography variant="h6" sx={{ fontFamily: "Poppins,sans-serif", color: "#01D28E", fontWeight: "semibold" }}>{foodTitle}</Typography>
-                    <Typography variant="body2" sx={{ fontFamily: "Poppins, sans-serif", fontSize: "0.65rem" }}>{foodDescription}</Typography>
+                <CardContent sx={{ height: "40%", "&:hover" : {color : "#ffffff"} }}>
+                    <Typography variant="h6" sx={{ fontFamily: "Poppins,sans-serif", fontWeight: "semibold"}}>{foodTitle}</Typography>
+                    <Typography variant="body2" sx={{ fontFamily: "Poppins, sans-serif", color : "black",  fontSize: "0.65rem" }}>{foodDescription}</Typography>
                 </CardContent>
             </Card>
     );
--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -4,20 +4,44 @@ import { MenuList } from "../Constants/Menu";
 import { MenuCard } from "../Components/MenuCard";
 import { Grid } from "@mui/material";
 import { useState } from "react";
+import { styled } from "@mui/system";
 
 export const Menu = () => {
 
+    const StyledTextField = styled(TextField)(() => ({
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderWidth: "3px",
+            borderColor: "#01D28E",
+        },
+        "& .MuiInputLabel-root.Mui-focused": {
+            color: "#01D28E",
+        },
+        "& .MuiOutlinedInput-root": {
+            borderRadius: "20px",
+        },
+    }));
+
     const [searchTerm, setSearchTerm] = useState("");
+    const [category, setCategory] = useState("All");
 
     const filteredMenuList = MenuList.filter((food) => {
         return food.name.toLowerCase().includes(searchTerm.toLowerCase());
     });
 
+    const categoryFilteredMenuList = MenuList.filter((food) => {
+        if (category === "All")
+            return food;
+        else if (category === "Veg")
+            return food.category === category;
+        else if (category === "Non-veg")
+            return food.category === category;
+    });
+
     return (
-        <Container sx={{display : "flex", flexDirection : "column", justifyContent : "center", alignItems : "center"}}>
+        <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
             <Navbar />
             <Container sx={{ textAlign: "center" }}>
-                <Typography variant="h4" sx={{ fontFamily: "Poppins, sans-serif", fontWeight: "bold", color: "#01D28E", marginTop: "5rem", marginBottom: "5rem" }}> Explore Menu</Typography>
+                <Typography variant="h4" sx={{ fontFamily: "Poppins, sans-serif", fontWeight: "bold", color: "#000000", marginTop: "5rem", marginBottom: "3rem" }}> Explore Menu</Typography>
             </Container>
             <Container sx={{ width: "100%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
                 <TextField
@@ -25,23 +49,38 @@ export const Menu = () => {
                     sx={{
                         width: "60%",
                         outlineColor: "#01D28E",
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                            borderWidth: "3px",
+                            borderColor: "#01D28E",
+                        },
+                        "& .MuiInputLabel-root.Mui-focused": {
+                            color: "#01D28E",
+                        },
+                        "& .MuiOutlinedInput-root": {
+                            borderRadius: "20px",
+                        },
                     }}
                     onChange={(event) => {
                         setSearchTerm(event.target.value);
                     }}
                 />
             </Container>
-            <Container sx={{display : "flex", flexDirection : "row", marginTop : "1rem", marginBottom : "1rem", justifyContent : "center", alignItems : "center"}}>
-                <Button variant="text" sx={{fontFamily : "Poppins, sans-serif", fontSize : "1rem", color : "#01D28E"}}>All</Button>
-                <Button variant="text" sx={{fontFamily : "Poppins, sans-serif", fontSize : "1rem", color : "#01D28E"}}>Veg</Button>
-                <Button variant="text" sx={{fontFamily : "Poppins, sans-serif", fontSize : "1rem", color : "#01D28E"}}>Non-veg</Button>
+            <Container sx={{ display: "flex", flexDirection: "row", marginTop: "1.5rem", justifyContent: "space-evenly", alignItems: "center" }}>
+                <Button variant="text" sx={{ border: "1px solid #ffffff", fontFamily: "Poppins, sans-serif", fontSize: "1rem", color: "#01D28E", borderRadius: "15px", cursor: "pointer", "&:hover": { border: "1px solid #01D28E" }, "&:active": { border: "1px solid #01D28E", backgroundColor: "rgba(1, 210, 142, 0.7)" } }} onClick={() => { setCategory("All") }}>All</Button>
+                <Button variant="text" sx={{ border: "1px solid #ffffff", fontFamily: "Poppins, sans-serif", fontSize: "1rem", color: "#01D28E", borderRadius: "15px", cursor: "pointer", "&:hover": { border: "1px solid #01D28E" } }} onClick={() => { setCategory("Veg") }}>Veg</Button>
+                <Button variant="text" sx={{ border: "1px solid #ffffff", fontFamily: "Poppins, sans-serif", fontSize: "1rem", color: "#01D28E", borderRadius: "15px", cursor: "pointer", "&:hover": { border: "1px solid #01D28E" } }} onClick={() => { setCategory("Non-veg") }}>Non-veg</Button>
             </Container>
-            <Container sx={{display : "flex", flexDirection : "row", justifyContent : "center", alignItems : "center", width : "100%"}}>
-                <Grid container spacing={3} sx={{ marginTop: "6rem", marginBottom: "3rem" }}>
+            <Container sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", width: "100%" }}>
+                <Grid container spacing={2.5} sx={{ marginTop: "3rem", marginBottom: "3rem" }}>
                     {searchTerm ? filteredMenuList.map((food, index) => (
                         <Grid item xs={12} sm={6} md={4} lg={4} key={index}>
                             <MenuCard foodTitle={food.name} foodDescription={food.description} foodImage={food.image} />
                         </Grid>
+                    )) : category ? categoryFilteredMenuList.map((food, index) => (
+                        <Grid item xs={12} sm={6} md={4} lg={4} key={index}>
+                            <MenuCard foodTitle={food.name} foodDescription={food.description} foodImage={food.image} />
+                        </Grid>
+
                     )) : MenuList.map((food, index) => (
                         <Grid item xs={12} sm={6} md={4} lg={4} key={index}>
                             <MenuCard foodTitle={food.name} foodDescription={food.description} foodImage={food.image} />
